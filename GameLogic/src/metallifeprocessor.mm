82124961583206ba648e68d84f2273784c514574
Arsonist <gmoximko@icloud.com>
2019-06-15 13:52:43 +0700

    GPU optimization

--- GameLogic/src/metallifeprocessor.mm
+++ GameLogic/src/metallifeprocessor.mm
@@ -5,6 +5,7 @@
 
 static NSString* const kernel_src =
     @"#include <metal_stdlib>\n"
+    "#include <metal_integer>\n"
 
     "using namespace metal;\n"
 
@@ -23,52 +24,85 @@ static NSString* const kernel_src =
     "  return ushort2((x + WIDTH) % WIDTH, (y + HEIGHT) % HEIGHT); "
     "}\n"
 
-    "uchar lifeBit(const device uchar* input, uint pos) "
+    "template<uint Bit> "
+    "struct Mask "
     "{ "
-    "  uint byte = pos >> 3; "
-    "  uint bit = pos & 7; "
-    "  return (input[byte] >> bit) & 1; "
+    "  constant constexpr static uint c_n_e_s_w = 0x70007 << (Bit - 1); "
+    "  constant constexpr static uint c_nw_ne_se_sw = 0x0; "
+    "  constant constexpr static uint c_self = 0x5 << (Bit - 1); "
+    "};\n"
+    "template<> "
+    "struct Mask<0> "
+    "{ "
+    "  constant constexpr static uint c_n_e_s_w = 0x80030003; "
+    "  constant constexpr static uint c_nw_ne_se_sw = 0x80000080; "
+    "  constant constexpr static uint c_self = 0x2; "
+    "};\n"
+    "template<> "
+    "struct Mask<7> "
+    "{ "
+    "  constant constexpr static uint c_n_e_s_w = 0xC001C0; "
+    "  constant constexpr static uint c_nw_ne_se_sw = 0x10100; "
+    "  constant constexpr static uint c_self = 0x40; "
+    "};\n"
+
+    "template<uint Bit>"
+    "uint isAlive(uint self, uint n_e_s_w, uint nw_ne_se_sw) "
+    "{ "
+    "/*"
+    "  nw0       n0        ne1 "
+    "  [|||||||0][00333|77][7|||||||] "
+    "w3[|||||||0][*03*3|7*][7|||||||]e1 "
+    "  [|||||||0][00333|77][7|||||||] "
+    "  sw3       s2        se2 "
+    "*/"
+    "  uint neighbours = popcount(Mask<Bit>::c_n_e_s_w & n_e_s_w) "
+    "    + popcount(Mask<Bit>::c_nw_ne_se_sw & nw_ne_se_sw) "
+    "    + popcount(Mask<Bit>::c_self & self); "
+
+    "  return static_cast<uint>((self >> Bit & 1) == 0 "
+    "    ? neighbours == 3 "
+    "    : neighbours == 2 || neighbours == 3) << Bit;"
     "}\n"
 
-    "uchar isAlive(const device uchar* input, uint id) "
+    "template<uint Bit>"
+    "uint calculateLife(uint self, uint n_e_s_w, uint nw_ne_se_sw) "
     "{ "
-    "  ushort2 gid = pos(id); "
-    "  uint nw = idx(loopPos(gid.x - 1, gid.y + 1)); "
-    "  uint n  = idx(loopPos(gid.x,     gid.y + 1)); "
-    "  uint ne = idx(loopPos(gid.x + 1, gid.y + 1)); "
-    "  uint e  = idx(loopPos(gid.x + 1, gid.y    )); "
-    "  uint se = idx(loopPos(gid.x + 1, gid.y - 1)); "
-    "  uint s  = idx(loopPos(gid.x    , gid.y - 1)); "
-    "  uint sw = idx(loopPos(gid.x - 1, gid.y - 1)); "
-    "  uint w  = idx(loopPos(gid.x - 1, gid.y    )); "
-
-    "  uchar neighbours = lifeBit(input, nw) "
-    "    + lifeBit(input, n) "
-    "    + lifeBit(input, ne) "
-    "    + lifeBit(input, e) "
-    "    + lifeBit(input, se) "
-    "    + lifeBit(input, s) "
-    "    + lifeBit(input, sw) "
-    "    + lifeBit(input, w); "
-
-    "  uchar self = lifeBit(input, id); "
-    "  return static_cast<uchar>((self == 0 && neighbours == 3) "
-    "    || (self == 1 && (neighbours == 2 || neighbours == 3))); "
+    "  return isAlive<Bit>(self, n_e_s_w, nw_ne_se_sw) "
+    "    | calculateLife<Bit - 1>(self, n_e_s_w, nw_ne_se_sw); "
+    "}\n"
+    "template<>"
+    "uint calculateLife<0>(uint self, uint n_e_s_w, uint nw_ne_se_sw)"
+    "{"
+    "  return isAlive<0>(self, n_e_s_w, nw_ne_se_sw); "
     "}\n"
 
-    "kernel void lifeStep(const device uchar* input [[buffer(0)]], "
-                         "      device uchar* output [[buffer(1)]], "
+    "kernel void lifeStep(constant uchar* input [[buffer(0)]], "
+                         "  device uchar* output [[buffer(1)]], "
                          "uint id [[thread_position_in_grid]]) "
     "{ "
-    "  uint bit = id * 8; "
-    "  output[id] = isAlive(input, bit + 0) << 0 "
-    "    | isAlive(input, bit + 1) << 1 "
-    "    | isAlive(input, bit + 2) << 2 "
-    "    | isAlive(input, bit + 3) << 3 "
-    "    | isAlive(input, bit + 4) << 4 "
-    "    | isAlive(input, bit + 5) << 5 "
-    "    | isAlive(input, bit + 6) << 6 "
-    "    | isAlive(input, bit + 7) << 7; "
+    "  ushort2 gid = pos(id * 8); "
+    "  uint nw = idx(loopPos(gid.x - 8, gid.y + 1)); "
+    "  uint n  = idx(loopPos(gid.x,     gid.y + 1)); "
+    "  uint ne = idx(loopPos(gid.x + 8, gid.y + 1)); "
+    "  uint e  = idx(loopPos(gid.x + 8, gid.y    )); "
+    "  uint se = idx(loopPos(gid.x + 8, gid.y - 1)); "
+    "  uint s  = idx(loopPos(gid.x    , gid.y - 1)); "
+    "  uint sw = idx(loopPos(gid.x - 8, gid.y - 1)); "
+    "  uint w  = idx(loopPos(gid.x - 8, gid.y    )); "
+
+    "  uint self = static_cast<uint>(input[id]); "
+    "  uint n_e_s_w = static_cast<uint>(input[n >> 3]) << 0 * 8 "
+    "    | static_cast<uint>(input[e >> 3]) << 1 * 8 "
+    "    | static_cast<uint>(input[s >> 3]) << 2 * 8 "
+    "    | static_cast<uint>(input[w >> 3]) << 3 * 8; "
+
+    "  uint nw_ne_se_sw = static_cast<uint>(input[nw >> 3]) << 0 * 8 "
+    "    | static_cast<uint>(input[ne >> 3]) << 1 * 8 "
+    "    | static_cast<uint>(input[se >> 3]) << 2 * 8 "
+    "    | static_cast<uint>(input[sw >> 3]) << 3 * 8; "
+
+    "  output[id] = static_cast<uchar>(calculateLife<7>(self, n_e_s_w, nw_ne_se_sw)); "
     "}\n";
 
 @interface MetalLifeProcessor : NSObject
@@ -136,7 +170,16 @@ static NSString* const kernel_src =
   id<MTLFunction> function = [library newFunctionWithName: @"lifeStep"];
   assert(function);
 
-  pipeline_state_ = [device newComputePipelineStateWithFunction: function error: &error];
+  MTLComputePipelineDescriptor* descriptor = [MTLComputePipelineDescriptor new];
+  descriptor.computeFunction = function;
+  descriptor.buffers[0].mutability = MTLMutabilityImmutable;
+  descriptor.buffers[1].mutability = MTLMutabilityMutable;
+
+  pipeline_state_ = [device
+      newComputePipelineStateWithDescriptor: descriptor
+      options: MTLPipelineOptionNone
+      reflection: nil
+      error: &error];
   assert([error code] == 0);
   assert(pipeline_state_);
 
